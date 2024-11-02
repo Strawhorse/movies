291a10ba7614ab81e8446858f4695a4ef215ede2
John Bracken <strawhorse@gmail.com>
2024-11-02 11:53:05 +0000

    Created Movie class to match movies document on database

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# movies-reviewer-sull-stack
- Full Stack application using Java Spring Boot, MongoDB (and MongoDB Compass for gui) for back-end and React for front-end
+# movies-reviewer-full-stack
+ 
+Full Stack application using Java Spring Boot, MongoDB (and MongoDB Compass for gui) for back-end and React for front-end
 Movie reviewer application 
+
 Other technologies used:
 - Mongo Atlas for Cloud backup and sharing
-- Lombok for code replication
+- Lombok for code annotation
 
 Some ideas taken from: https://www.youtube.com/watch?v=5PdEmeopJVQ
+
 Atlas URL: https://cloud.mongodb.com
 
+Created MongoDB account and set up cluster first, then created back end and added mappings.
+
+Finally created limited front end using React
--- src/main/java/dev/johnbr/movies/Movie.java
+++ src/main/java/dev/johnbr/movies/Movie.java
@@ -1,5 +1,34 @@
 package dev.johnbr.movies;
 
+//Lombok import takes care of getters and setters
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import java.util.List;
+
+//Annotate as a document to tell framework that this class represents each document in the movies collection
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
-    
+
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+//    private List<String> reviewIds;
+
+
 }
